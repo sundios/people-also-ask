93a459010b86ea1cb9db32bf6e208ffb454cbf0a
sundios <sundios@gmail.com>
2020-05-01 00:45:59 -0400

    fixed question by index

--- test1.py
+++ test1.py
@@ -1,41 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 23:28:11 2020
+
+@author: konradburchardt
+"""
+
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from progress.bar import Bar
-
-import itertools
-import threading
 import time
-import sys
+import logging
+
 
 query = input("Add query: ")
 clicks = input("How many questions do you want to click? : ")
 clicks = int(clicks)  # parse string into an integer
 
 
-#This is only a loading bar
-done = False
-def animate():
-    for c in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rloading ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\rDone!     ')
 
 
-#Search Query and get results
-
-options = Options()
-options.headless = True
-
-def search(query):
-       t = threading.Thread(target=animate)
-       t.start()
-       with webdriver.Firefox(options=options) as driver:
+def search(query,clicks):
+       with webdriver.Firefox() as driver:
             
             driver.get("https://www.google.com?hl=en")
             driver.find_element_by_xpath("//input[@aria-label='Search']").send_keys(query)
@@ -43,10 +32,8 @@ def search(query):
             searchbtn = driver.find_elements_by_xpath("//input[@aria-label='Google Search']")
             searchbtn[-1].click()
             
-            #Questions separately
-            paa = driver.find_elements_by_xpath("//span/following-sibling::div[contains(@class,'match-mod-horizontal-padding')]")
+            #clicking Question 1 from here we need to figure out how to click each question and save childrens
             
-    
             
             for i in range(clicks):
                 print(i)
@@ -54,13 +41,43 @@ def search(query):
                 paa = driver.find_elements_by_xpath("//span/following-sibling::div[contains(@class,'match-mod-horizontal-padding')]")
                 for j in paa:
                     print(format(j.text))
+
+search(query,clicks)
+
+
+
+
+            
         
 
     
-search(query)
-#loading bar finishes
-done = True 
+
     
+
+
+
+
+#### This is for guidance
+#
+#xpath("(//*[@id='menuitem'])[index]")
+#
+#
+##x path by index??
+#(//div[@class='className'])[1]
+#(//div[@class='className'])[2]
+#(//div[@class='className'])[3]
+#(//div[@class='className'])[4]
+#(//div[@class='className'])[5]
+#
+#
+#
+#persons = []
+#for person in driver.find_elements_by_class_name('person'):
+#    title = person.find_element_by_xpath('.//div[@class="title"]/a').text
+#    company = person.find_element_by_xpath('.//div[@class="company"]/a').text
+#
+#    persons.append({'title': title, 'company': company})
+#
 
 
 
